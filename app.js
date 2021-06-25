f3dc91280aff92211cb9b4525fe24cce0b86364b
Muhammad Yahiya <64965102+Yahiya166@users.noreply.github.com>
2021-06-25 23:25:02 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,167 @@
+// ******* Chapter #1 ************
+
+// ******* Q1 ************
+
+// alert('Hello World');
+
+
+// ******* Q2 ************
+
+// alert('Error! Please enter a valid password');
+
+
+// ******* Q3 ************
+
+// alert('Welcome to JS Land... \r\n Happy Coding');
+
+
+// ******* Q4 ************
+
+// alert('Welcome to JS Land...');
+
+
+// ******* Q5 ************
+
+// console.log("Heloo... I can run Js Through my web Browser's console");
+// alert("Heloo... I can run Js Through my web Browser's console");
+
+
+// ******* Q5 ************
+// ******* Q6 ************
+
+// Completed
+
+
+
+
+// ******* Chapter #2 ************
+
+// ******* Q1 ************
+
+// var username = ;
+
+
+// ******* Q2 ************
+
+// var myName = "Muhammad Yahiya Asif";
+
+
+// ******* Q3 ************
+
+// var message = "Hello World";
+
+// alert(message);
+
+
+// ******* Q4 ************
+
+// var myName = "Yahiya Asif";
+// var age = "15 Years old";
+// var subject = "Certified Mobile Application Development";
+
+// alert(myName);
+// alert(age);
+// alert(subject);
+
+
+// ******* Q5 ************
+
+// alert('PIZAA \r\n PIZZ \r\n PIZ \r\n PI \r\n P');
+
+
+// ******* Q6 ************
+
+// var email = "muhammadyahiya166@gmail.com";
+
+// alert('My email address is  ' + email);
+
+
+// ******* Q7 ************
+
+// var book = "A smarter way to learn JavaScript";
+
+// alert('I am trying to learn from the book  ' + book);
+
+
+// ******* Q8 ************
+
+// document.write("Yah! I can write HTML content through JavaScript");
+
+
+
+// ******* Q9 ************
+
+// var show = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+
+// alert(show);
+
+
+
+
+
+// ******* Chapter #3 ************
+
+// ******* Q1 ************
+
+// var age = 17;
+
+// alert('I am ' + age + ' years old');
+
+
+
+// ******* Q2 ************
+
+// alert('You have visited this site N times');
+
+
+
+// ******* Q3 ************
+
+// birthYear = 2004;
+
+// document.write("My birth year is " + birthYear);
+
+
+
+// ******* Q4 ************
+
+// var clothing = "xyzClothing";
+// var visitor_name = "Yahiya";
+// var Product = "T-Shirt(s)";
+// var quantity = 5;
+
+// document.write(visitor_name + ' Ordered ' +  quantity +  Product + ' on ' + clothing);
+
+
+
+
+
+// ******* Chapter #4 ************
+
+// ******* Q1 ************
+
+// var variable1 = "", variable2 = '', variable3 = '';
+
+
+
+// ******* Q2 ************
+
+// var HelloWorld = '', name = '', father_name = '', $variable = '', variable123 = '';
+// var Hello-World = '', #name = '', #father+name = '', !variable = '', 123variable = '';
+
+
+
+// ******* Q3 ************
+
+// document.write("Rules for naming JS variables");
+// document.write("<br>");
+// document.write("<br>");
+// document.write("<br>");
+// document.write("Variable names can only contain $my_1stVariable <br> Variables must begin with a name, $name or _name <br> Variable names are case Sensitive <br>Variable names should not be JS Keyboard");
+
+
+
+
+
+
+// ******* Chapter #5 ************
